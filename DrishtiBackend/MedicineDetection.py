979ada135585b7289e4ddf8a2a3d26ccf4694112
Saleh Ahmed <salehahmed@Salehs-MacBook-Pro.local>
2022-12-19 19:42:35 +0600

    Currency Detection Deployed

--- DrishtiBackend/MedicineDetection.py
+++ DrishtiBackend/MedicineDetection.py
@@ -1,8 +1,8 @@
-# GitHub repo installation of paddle
-import PaddleOCR
-import cv2
-import plt
-import os
+
+from paddleocr import PaddleOCR, draw_ocr 
+from matplotlib import pyplot as plt 
+import cv2 
+import os 
 
 ocr_model = PaddleOCR(lang='en')
 img_path='Drishti\DrishtiApp\Medicine Detection\\testimg.png'
@@ -17,7 +17,7 @@ scores = [res[1][1] for res in result]
 font_path = os.path.join('PaddleOCR', 'doc', 'fonts', 'latin.ttf')
 img = cv2.imread(img_path) 
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
-plt.figure(figsize=(15,15))
+# plt.figure(figsize=(15,15))
 annotated = draw_ocr(img, boxes, texts, scores, font_path=font_path) 
-plt.imshow(annotated) 
+# plt.imshow(annotated) 
 img.shape
