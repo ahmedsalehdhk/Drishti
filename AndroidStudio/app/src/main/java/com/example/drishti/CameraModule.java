c29a587c8ee5bd618865abb2a27d8484e3cb2a8f
Saleh Ahmed <salehahmed@Salehs-MacBook-Pro.local>
2022-12-21 17:03:23 +0600

    Image Resolved

---
+++ AndroidStudio/app/src/main/java/com/example/drishti/CameraModule.java
@@ -0,0 +1,104 @@
+package com.example.drishti;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+
+public class CameraModule extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_module);
+
+        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+        Button photoButton = (Button) this.findViewById(R.id.capture_btn);
+        photoButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                    {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                    }
+                    else
+                    {
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    }
+                }
+            }
+        });
+
+
+        //Back Button
+        Button camera_back_btn = findViewById(R.id.camera_module_back_btn);
+        camera_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goBack = new Intent(CameraModule.this, ModeSelection2a.class);
+                startActivity(goBack);
+            }
+        });
+
+    } //on create ends here
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+            else
+            {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream .toByteArray();
+            String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+            TextView line =(TextView)findViewById(R.id.line);
+            line.setText(encoded);
+        }
+    }
+
+
+}
--- AndroidStudio/app/src/main/java/com/example/drishti/ModeSelection2a.java
+++ AndroidStudio/app/src/main/java/com/example/drishti/ModeSelection2a.java
@@ -1,12 +1,23 @@
 package com.example.drishti;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ModeSelection2a extends AppCompatActivity {
 
@@ -15,7 +26,7 @@ public class ModeSelection2a extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_selection2a);
 
-        //moving back to ModeSelection1
+        //Back Button Code
         Button back_btn_2a = (Button)findViewById(R.id.back_btn_2a);
         back_btn_2a.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,19 +36,19 @@ public class ModeSelection2a extends AppCompatActivity {
             }
         });
 
-        //opening camera
-        Button photoButton = (Button)findViewById(R.id.object_btn);
-        ImageView imageView = findViewById(R.id.imageViewtest);
-
-
-
-
-
-
-
-
+        //Opening Camera
+        Button currency_btn = (Button)findViewById(R.id.currency_btn);
+        currency_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent currency_btn_intent = new Intent(ModeSelection2a.this, CameraModule.class);
+                startActivity(currency_btn_intent);
+            }
+        });
 
 
 
     }
-}
+    //on create ends here
+
+}   //end of main function
