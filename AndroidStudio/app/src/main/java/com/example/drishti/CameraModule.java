1339004b2b5d8fc9eaccf1901af311a934307744
Saleh Ahmed <salehahmed@Salehs-MacBook-Pro.local>
2022-12-21 21:51:04 +0600

    Made Android Studio Only

--- AndroidStudio/app/src/main/java/com/example/drishti/CameraModule.java
+++ AndroidStudio/app/src/main/java/com/example/drishti/CameraModule.java
@@ -10,7 +10,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.app.Activity;
@@ -20,19 +19,36 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class CameraModule extends AppCompatActivity {
 
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private String url="http://10.0.2.2:3000";
+    private String POST="POST";
+    private String GET="GET";
+    public static String responseData;
+    public Button helpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_module);
 
+
         this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+        helpBtn = (Button)findViewById(R.id.help);
         Button photoButton = (Button) this.findViewById(R.id.capture_btn);
         photoButton.setOnClickListener(new View.OnClickListener()
         {
@@ -64,6 +80,8 @@ public class CameraModule extends AppCompatActivity {
             }
         });
 
+
+
     } //on create ends here
 
     @Override
@@ -74,7 +92,6 @@ public class CameraModule extends AppCompatActivity {
         {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
             {
-                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
@@ -96,9 +113,63 @@ public class CameraModule extends AppCompatActivity {
             byte[] byteArray = byteArrayOutputStream .toByteArray();
             String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
             TextView line =(TextView)findViewById(R.id.line);
-            line.setText(encoded);
+            {
+                helpBtn.setOnClickListener(view -> {
+//                    sendRequest(GET,"gettext","1","1&1");
+                    responseData = "Saleh";
+                    Intent nextP = new Intent(this, Results1a.class);
+                    startActivity(nextP);
+                });
+            }
+            line.setText(responseData);
         }
     }
 
+
+    void sendRequest(String type,String method,String paramname,String param){
+
+        /* if url is of our get request, it should not have parameters according to our implementation.
+         * But our post request should have 'name' parameter. */
+        String fullURL=url+"/"+method+(param==null?"":"/"+param);
+        Request request;
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS).build();
+
+        /* If it is a post request, then we have to pass the parameters inside the request body*/
+        if(type.equals(POST)){
+            RequestBody formBody = new FormBody.Builder()
+                    .add(paramname, param)
+                    .build();
+
+            request=new Request.Builder()
+                    .url(fullURL)
+                    .post(formBody)
+                    .build();
+        }else{
+            /*If it's our get request, it doen't require parameters, hence just sending with the url*/
+            request = new Request.Builder()
+                    .url(fullURL)
+                    .build();
+        }
+        /* this is how the callback get handled */
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                // Read data on the worker thread
+                responseData = response.body().string();
+                Intent nextPage = new Intent(CameraModule.this, Results1a.class);
+                startActivity(nextPage);
+            }
+
+        });
+    }
 
 }
--- AndroidStudio/app/src/main/java/com/example/drishti/Results1a.java
+++ AndroidStudio/app/src/main/java/com/example/drishti/Results1a.java
@@ -3,6 +3,8 @@ package com.example.drishti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class Results1a extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Results1a extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results1a);
+
+        TextView result = findViewById(R.id.ResultTextView);
+        String Str = CameraModule.responseData;
+        result.setText(Str);
+        Log.v("test", Str);
     }
 }
